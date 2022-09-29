12046e1742b9cdc09a9d5092eda0870671cda379
dekm <gevan73@gmail.com>
2022-09-29 12:37:13 +0200

    unigrid.sh start script

---
+++ unigrid.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+# shellcheck disable=SC2034
+# Copyright © 2021-2022 The Unigrid Foundation, UGD Software AB
+
+# This program is free software: you can redistribute it and/or modify it under the terms of the
+# addended GNU Affero General Public License as published by the Free Software Foundation, version 3
+# of the License (see COPYING and COPYING.addendum).
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+
+# You should have received an addended copy of the GNU Affero General Public License with this program.
+# If not, see <http://www.gnu.org/licenses/> and <https://github.com/unigrid-project/unigrid-installer>.
+
+: '
+# Run this file
+
+```
+bash -ic "$(wget -4qO- -o- raw.githubusercontent.com/unigrid-project/unigrid-installer/master/unigrid.sh)" ; source ~/.bashrc
+```
+
+'
+
+# Github user and project.
+GITHUB_REPO='unigrid-project/unigrid-installer'
+# Display Name.
+DAEMON_NAME='UNIGRID'
+# Coin Ticker.
+TICKER='UGD'
+# Binary base name.
+BIN_BASE='unigrid'
+# Directory.
+DIRECTORY='.unigrid'
+# Conf File.
+CONF='unigrid.conf'
+# Port.
+DEFAULT_PORT=51992
+# Explorer URL.
+EXPLORER_URL='http://explorer.unigrid.org/'
+# Rate limit explorer.
+EXPLORER_SLEEP=1
+# Amount of Collateral needed.
+COLLATERAL=3000
+# Blocktime in seconds.
+BLOCKTIME=60
+# Multiple on single IP.
+MULTI_IP_MODE=0
+
+
+ASCII_ART () {
+echo -e "\e[0m"
+clear 2> /dev/null
+cat << "UNIGRID"
+ _   _ _   _ ___ ____ ____  ___ ____
+| | | | \ | |_ _/ ___|  _ \|_ _|  _ \
+| | | |  \| || | |  _| |_) || || | | |
+| |_| | |\  || | |_| |  _ < | || |_| |
+ \___/|_| \_|___\____|_| \_\___|____/
+
+UNIGRID
+}
+
+cd ~/ || exit
+COUNTER=0
+rm -f ~/___mn.sh
+while [[ ! -f ~/___mn.sh ]] || [[ $( grep -Fxc "# End of gridnode setup script." ~/___mn.sh ) -eq 0 ]]
+do
+  rm -f ~/___mn.sh
+  echo "Downloading Unigrid Setup Script."
+  wget -4qo- raw.githubusercontent.com/unigrid-project/unigrid-installer/master/setup.sh -O ~/___mn.sh
+  COUNTER=1
+  if [[ "${COUNTER}" -gt 3 ]]
+  then
+    echo
+    echo "Download of setup script failed."
+    echo
+    exit 1
+  fi
+done
+
+(
+  sleep 2
+  rm ~/___mn.sh
+) & disown
+
+(
+# shellcheck disable=SC1091
+# shellcheck source=/root/___mn.sh
+. ~/___mn.sh
+DAEMON_SETUP_THREAD
+)
+# shellcheck source=/root/.bashrc
+. ~/.bashrc
+stty sane 2>/dev/null
+
