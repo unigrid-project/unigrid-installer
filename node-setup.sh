5153764dcc306be5fc154d6d06d0c6d947d9f05f
dekm <gevan73@gmail.com>
2022-10-04 18:12:14 +0200

    add default name and start container detatched

--- node-setup.sh
+++ node-setup.sh
@@ -14,32 +14,45 @@
 # You should have received an addended copy of the GNU Affero General Public License with this program.
 # If not, see <http://www.gnu.org/licenses/> and <https://github.com/unigrid-project/unigrid-installer>.
 
+BASE_NAME='ugd_docker_'
+SERVER_NAME=''
+
 echo "Starting Docker Instll Script"
 
 if [ ! -x "$( command -v docker )" ]
 then
 bash <(wget -qO- https://raw.githubusercontent.com/docker/docker-install/master/install.sh)
 sudo chmod 666 /var/run/docker.sock
+sudo groupadd docker
+CURRENT_USER=$(whoami)
+echo ${CURRENT_USER}
+sudo usermod -a -G docker ${CURRENT_USER}
 echo "Complete Docker Install"
 fi
 
-SERVER_NAME=${1}
 if [ ! -x '$( docker ps -a --no-trunc --format "{{.Mounts}}" )' ]
 then
-docker run -it --name="${SERVER_NAME}" --mount source=data-volume,destination=/root/.unigrid unigrid/unigrid:beta
+SERVER_NAME="${BASE_NAME}1"
+docker run -it --name="${SERVER_NAME}" --mount source=data-volume_1,destination=/root/.unigrid unigrid/unigrid:beta
 else
+SERVER_NAME=$(docker ps -a --no-trunc --format '{{.Names}}')
+echo ${SERVER_NAME}
+A="$(echo ${SERVER_NAME} | cut -d'_' -f3)"
+B="$(($A + 1))"
+echo ${BASE_NAME}${B}
 echo "Copy Volume and run"
 docker run --rm \
            -i \
+           -d \
            -t \
            -v data-volume:/from \
            -v data-volume4:/to \
            alpine ash -c "cd /from ; cp -av . /to"
 echo "Done copying volume"
-docker run -it --name=test4 --mount source=data-volume4,destination=/root/.unigrid unigrid/unigrid:beta
+docker run -it -d --name="${SERVER_NAME}" --mount source=data-volume4,destination=/root/.unigrid unigrid/unigrid:beta
 fi
 
-CURRENT_CONTAINER_ID=$( echo `sudo docker ps -aqf name="${somedrive}"` )
+CURRENT_CONTAINER_ID=$( echo `sudo docker ps -aqf name="${SERVER_NAME}"` )
 echo "${CURRENT_CONTAINER_ID}"
 docker start "${CURRENT_CONTAINER_ID}"
 echo "Starting ${CURRENT_CONTAINER_ID}"
